df0d5c432e2a0c96c74b4799a9202c0777da1428
mstr_amirmahdavi <seyedamir_mahdavi@yahoo.com>
2019-03-11 14:12:58 +0330

    home page ready to be responsive

---
+++ assets/js/cusstom.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+  $('#owl-carousel').owlCarousel({
+    rtl: true,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 2
+      }
+    }
+  });
+  $('#mainsliders').owlCarousel({
+    loop: true,
+    slideSpeed: 300,
+    paginationSpeed: 1000,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    singleItem: true,
+    pagination: false,
+    rewindSpeed: 500,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  });
+  $('#extraslider').owlCarousel({
+    loop: true,
+    margin: 10,
+    center: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  });
+})
